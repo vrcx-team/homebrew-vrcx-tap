65558ab3d948931b535ed06459756d0c100757d3
Natsumi <Natsumi-sama@users.noreply.github.com>
2026-02-26 11:51:21 +0000

    vrcx@nightly 2026-02-26T11.38-87f1aee

--- Casks/vrcx@nightly.rb
+++ Casks/vrcx@nightly.rb
@@ -1,9 +1,9 @@
 cask "vrcx@nightly" do
   arch arm: "arm64", intel: "x64"
 
-  version "2026-02-24T13.11-3639f4c"
-  sha256 arm:   "e6bc652173c47191852438dba52672c1c1df71980ecb33259959fb0f8c4d083d",
-         intel: "2f5c043b31c7a5bf3b89fe75216574ad0f6406b246eb42d72fb38b55d61840e2"
+  version "2026-02-26T11.38-87f1aee"
+  sha256 arm:   "654b59f6c5995551b7cf8116f4b87d7d7a8b0106c4c143a1049a40cdcbf9e67c",
+         intel: "6d3f98f259840dfef6b187d9e4d6142463316fa80fc01cf7e38b062b404824bb"
 
   url "https://github.com/Natsumi-sama/VRCX/releases/download/#{version}/VRCX_#{version}_#{arch}.dmg",
       verified: "github.com/Natsumi-sama/VRCX/"
